591681b809f53e66bdec61815ceabbca1aaa3d0d
Alicja Namieta <anamieta@2-H-5.42heilbronn.de>
2024-05-02 18:20:13 +0200

    path utils

---
+++ bonus/path_utils.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 18:19:50 by anamieta          #+#    #+#             */
+/*   Updated: 2024/05/02 18:19:59 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "pipex.h"
+
+int	path_index(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T'
+			&& envp[i][3] == 'H' && envp[i][4] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	**splitting_paths(char **envp)
+{
+	int		i;
+	char	**paths;
+
+	i = path_index(envp);
+	if (i == -1)
+		return (NULL);
+	paths = ft_split(envp[i] + 5, ':');
+	return (paths);
+}
+
+char	*find_path(char **envp, char *argv)
+{
+	char	**paths;
+	char	*cmd;
+	char	*aux;
+	int		i;
+
+	i = 0;
+	paths = splitting_paths(envp);
+	while (paths[i])
+	{
+		aux = ft_strjoin(paths[i], "/");
+		cmd = ft_strjoin(aux, argv);
+		free(aux);
+		if (access(cmd, X_OK) == 0)
+			return (free_array(paths), cmd);
+		free(cmd);
+		i++;
+	}
+	return (free_array(paths), NULL);
+}
