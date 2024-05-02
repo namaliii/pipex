0fac4f5a9ae240b68ad8bef71b467a76178b2959
Alicja Namieta <anamieta@2-H-5.42heilbronn.de>
2024-05-02 18:28:56 +0200

    utils added

---
+++ bonus/utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 18:28:34 by anamieta          #+#    #+#             */
+/*   Updated: 2024/05/02 18:28:47 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	length;
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+	return (length);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*constr;
+	size_t	lens1;
+	size_t	lens2;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	constr = (char *)ft_calloc((lens1 + lens2 + 1), sizeof(char));
+	if (constr == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		constr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		constr[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (constr);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, sizeof(c));
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	len;
+
+	if (s == NULL)
+		return ;
+	len = ft_strlen(s);
+	write(fd, s, len);
+}
